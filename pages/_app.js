fc6a243fcbfc04986a1a605d9ddbc31ba52058f0
Dev Arora <aroradev1010@gmail.com>
2024-09-12 12:55:26 +0530

    update favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout.js";
 import Transition from "../components/Transition.js";
-
+import Head from "next/head";
 //router
 import { useRouter } from "next/router";
 
@@ -16,6 +16,10 @@ function MyApp({ Component, pageProps }) {
       <AnimatePresence mode="wait">
         <motion.div key={router.route} className="h-full">
           <Transition />
+          <Head>
+            <title>Dev | Interactive Portfolio</title>
+            <link rel="icon" href="/logo.png" />
+          </Head>
           <Component {...pageProps} />
         </motion.div>
       </AnimatePresence>
